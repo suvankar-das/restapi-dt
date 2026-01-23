d7d520cc55151e89e1e615521ec1499d22dc0cc1
Suvankar Das <hi.suvankar@gmail.com>
2026-01-23 10:00:54 +0530

    movie mapper without using automapper

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Application.Requests;
+using Movies.Application.Responses;
+
+namespace Movies.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        [HttpPost("movies")]
+        public async Task<IActionResult> CreateMovie([FromBody] CreateMovieRequest movieRequest)
+        {
+            var movieCreate = movieRequest.CreateMovieRequestToMovie();
+            await _movieRepository.CreateMovieAsync(movieCreate);
+            var response = movieCreate.CreateMovieResponseFromMovie();
+            return Created($"/api/movies/{movieCreate.Id}", response);
+        }
+    }
+}
---
+++ Movies.Api/Mapping/ContractMapping.cs
@@ -0,0 +1,30 @@
+﻿using Movies.Application.Models;
+using Movies.Application.Requests;
+using Movies.Application.Responses;
+
+namespace Movies.Api.Mapping;
+
+public static class ContractMapping
+{
+    public static Movie CreateMovieRequestToMovie(this CreateMovieRequest createMovieRequest)
+    {
+        return new Movie()
+        {
+            Id = Guid.NewGuid(),
+            Genres = createMovieRequest.Genres.ToList(),
+            Title = createMovieRequest.Title,
+            YearOfRelease = createMovieRequest.YearOfRelease
+        };
+    }
+
+    public static MovieResponse CreateMovieResponseFromMovie(this Movie movie)
+    {
+        return new MovieResponse()
+        {
+            Id = movie.Id,
+            YearOfRelease = movie.YearOfRelease,
+            Genres = movie.Genres,
+            Title = movie.Title
+        };
+    }
+}
